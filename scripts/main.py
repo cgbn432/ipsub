7b0c63665ed31949cc342a185d2bc0028bca138d
cgbn432 <2224312860@qq.com>
2026-09-15 20:49:16 +0800

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -28,6 +28,7 @@ SOURCE_URLS = [
     "https://github.com/Au1rxx/free-vpn-subscriptions/raw/main/output/v2ray-base64.txt",
     "https://raw.githubusercontent.com/freefq/free/master/v2",
     "https://open.heleimail.workers.dev/",
+    "https://shadowmere.xyz/api/b64sub/",
     "https://www.ermao.net/sub/v2ray/ermao.net",
 ]
 
